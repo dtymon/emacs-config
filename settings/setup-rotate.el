0a4def289f7f36d064b4e8e1dd13a77540c67fee
David Tymon <david.tymon@bendigoadelaide.com.au>
2021-10-07 18:08:51 +1100

    Add window rotation

--- settings/setup-rotate.el
+++ settings/setup-rotate.el
@@ -1,4 +1,17 @@
 (use-package rotate
-  :ensure t)
+  :ensure t
+  :config
+  ;; Write a little function that does the rotation but keeps focus in the
+  ;; currently selected window rather than the focus following the buffer.
+  (defun dtymon::rotate-keep-focus()
+    (interactive)
+    (let ((curr-window (selected-window)))
+      (rotate-window)
+      (select-window curr-window)
+    ))
+  :bind (:map global-map
+              ("<S-kp-add>" . dtymon::rotate-keep-focus)
+              )
+  )
 
 (provide 'setup-rotate)
