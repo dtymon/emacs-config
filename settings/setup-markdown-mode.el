d791fc268c2677767f00906eca9cf9fbdcbbced8
David Tymon <david.tymon@gmail.com>
2021-10-15 05:49:56 +1100

    Configure markdown mode

--- settings/setup-markdown-mode.el
+++ settings/setup-markdown-mode.el
@@ -1,6 +1,11 @@
 (use-package markdown-mode
   :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
   :init
+  (setq markdown-command "multimarkdown")
   (setq markdown-imenu-generic-expression
         '(("title"  "^\\(.*\\)[\n]=+$" 1)
           ("h2-"    "^\\(.*\\)[\n]-+$" 1)
@@ -12,6 +17,15 @@
           ("h6"   "^###### \\(.*\\)$" 1)
           ("fn"   "^\\[\\^\\(.*\\)\\]" 1)
           ))
+  :custom
+  (markdown-hide-markup nil)
+  (markdown-bold-underscore t)
+  (markdown-italic-underscore t)
+  (markdown-header-scaling t)
+  (markdown-indent-function t)
+  (markdown-enable-math t)
+  (markdown-hide-urls nil)
+
   :config
   (add-hook 'markdown-mode-hook
             (lambda ()
