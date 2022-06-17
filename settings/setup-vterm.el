211c0c8a653d1c53d1afce4f5508f88e7d891201
David Tymon <david.tymon@gmail.com>
2022-06-17 17:00:25 +1000

    More configuration for vterm

--- settings/setup-vterm.el
+++ settings/setup-vterm.el
@@ -1,23 +1,25 @@
-
 (use-package vterm
   :ensure t
   :config
+  ;; (setq term-prompt-regexp "^\nzsh .*\n[0-9][0-9]:[0-9][0-9][AP]M .*> ")
+
   (defun dtymon::vterm-turn-off-chrome ()
     (hl-line-mode -1)
     (display-line-numbers-mode -1))
+
   :hook (vterm-mode . dtymon::vterm-turn-off-chrome)
   )
 
-(use-package vterm-toggle
-  :ensure t
-  :custom
-  (vterm-toggle-fullscreen-p nil "Open a vterm in another window.")
-  (vterm-toggle-scope 'projectile)
-  :bind (
-         ("C-c t" . #'vterm-toggle)
-         :map vterm-mode-map
-         ("s-t" . #'vterm) ; Open up new tabs quickly
-         )
-  )
+;; (use-package vterm-toggle
+;;   :ensure t
+;;   :custom
+;;   (vterm-toggle-fullscreen-p nil "Open a vterm in another window.")
+;;   (vterm-toggle-scope 'project)
+;;   :bind (
+;;          ("C-c t" . #'vterm-toggle)
+;;          :map vterm-mode-map
+;;          ("s-t" . #'vterm) ; Open up new tabs quickly
+;;          )
+;;   )
 
 (provide 'setup-vterm)
