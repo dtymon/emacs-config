bfeb4e9e80b2a34d4d141cc2fca82cc8afba42f1
David Tymon <david.tymon@gmail.com>
2024-05-07 20:42:02 +1000

    Add some more key bindings for mct

--- settings/setup-mct.el
+++ settings/setup-mct.el
@@ -12,12 +12,23 @@
   ;; Restore DEL back to just deleting a char
   (let ((map mct-minibuffer-local-completion-map))
     (define-key map (kbd "DEL") #'backward-delete-char)
+    ;; (define-key map (kbd "RET") #'mct-choose-completion-exit)
+    ;; (define-key map (kbd "RET") #'mct-choose-completion-dwim)
 
     ;; Undo remaps for arrow keys, they should go through the history
     (define-key map [remap next-line] nil)
     (define-key map [remap next-line-or-history-element] nil)
     (define-key map [remap previous-line] nil)
     (define-key map [remap previous-line-or-history-element] nil)
+
+    ;; Map C-s and C-r to be a bit like ido-switch-buffer except it doesn't
+    ;; search but goes back and forth through the history.
+    (define-key map (kbd "C-s") #'previous-line-or-history-element)
+    (define-key map (kbd "C-r") #'next-line-or-history-element)
+
+    ;; Rebind S-up and S-down to jump to the top/bottom of completions
+    (define-key map [remap windmove-up] #'mct-switch-to-completions-top)
+    (define-key map [remap windmove-down] #'mct-switch-to-completions-bottom)
     )
   )
 
