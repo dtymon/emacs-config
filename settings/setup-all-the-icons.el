79cbf125ac5873d0330fbdd7891d4397119964ef
David Tymon <david.tymon@gmail.com>
2024-05-07 12:46:21 +1000

    Cleanup dired configuration

--- settings/setup-all-the-icons.el
+++ settings/setup-all-the-icons.el
@@ -1,13 +1,15 @@
 (use-package all-the-icons
   :ensure t
+  :if (display-graphic-p)
   )
 
-(use-package all-the-icons-dired
-  :ensure t
-  )
-
-(use-package all-the-icons-completion
-  :ensure t
-  )
+;; I think I'm doing something wrong here because the icons shown when
+;; completing are not as pretty as those shown in say dired.
+;; (use-package all-the-icons-completion
+;;   :ensure t
+;;   :if (display-graphic-p)
+;;   :config
+;;   (all-the-icons-completion-mode)
+;;   )
 
 (provide 'setup-all-the-icons)
