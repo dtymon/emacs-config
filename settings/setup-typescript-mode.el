208f53c277a2d533e0f4d4ae281908244ac0c5c1
David Tymon <david.tymon@gmail.com>
2024-05-10 11:09:55 +1000

    Stop TS comment filling variables affecting other modes
    We need to make the setting of `comment-line-break-function` to
    be buffer-local.

--- settings/setup-typescript-mode.el
+++ settings/setup-typescript-mode.el
@@ -5,6 +5,9 @@
   :interpreter "node"
 
   :init
+  ;; We require js2-mode as we use its fill function
+  (require 'js2-mode)
+
   ;; Turn on tree-sitter and use it for highlighting
   (add-hook 'typescript-mode-hook #'tree-sitter-mode)
   (add-hook 'typescript-mode-hook #'tree-sitter-hl-mode)
@@ -24,13 +27,6 @@
                ;; This is the fill column for comments
                fill-column 80
 
-               ;; Don't use js2-mode's comment wrapping override functions as
-               ;; they don't indent properly. Instead use the standard C ones as
-               ;; they appear to work better.
-               ;;
-               ;; Actually revert back to js2-mode's comments as it seems to be
-               ;; working better these days.
-               comment-line-break-function #'js2-line-break
                ;; comment-line-break-function #'indent-new-comment-line
                ;; comment-line-break-function #'c-indent-new-comment-line
                js-indent-level 2
@@ -39,6 +35,17 @@
 
                ;; These code actions are very OTT
                lsp-ui-sideline-show-code-actions nil
+               )
+
+              ;; This needs to be set as a buffer-local var to avoid affecting
+              ;; modes other than TS.
+              ;;
+              ;; We use the js2-line-break function for filling because it
+              ;; handles both single-line and multi-line comments as required.
+              ;; The default TS fill function does not add a '*' prefix when
+              ;; wrapping multi-line comments.
+              (setq-local
+               comment-line-break-function #'js2-line-break
                )
 
               ;; Don't use the fill column from the prettier config
