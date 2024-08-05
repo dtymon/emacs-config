b6c9ba991cafcdaaacd9db43e351721ff42537d3
David Tymon <david.tymon@gmail.com>
2024-08-05 15:49:49 +1000

    Remove unused config for tree-sitter

--- settings/setup-tree-sitter.el
+++ settings/setup-tree-sitter.el
@@ -3,20 +3,6 @@
   :blackout tree-sitter-mode
   :init
   (global-tree-sitter-mode)
-
-  :config
-  ;; (set-face-attribute 'tree-sitter-hl-face:property nil :slant 'normal)
-  ;; (set-face-attribute 'tree-sitter-hl-face:function.call nil :weight 'normal)
-  ;; (set-face-attribute 'tree-sitter-hl-face:method.call nil :weight 'normal)
-  ;; (set-face-attribute 'tree-sitter-hl-face:string.special nil :weight 'normal)
-  ;;
-  ;; (set-face-foreground 'tree-sitter-hl-face:function.call
-  ;;                      (face-foreground 'font-lock-function-name-face))
-  ;; (set-face-foreground 'tree-sitter-hl-face:method.call
-  ;;                      (face-foreground 'font-lock-function-name-face))
-  ;; (set-face-foreground 'tree-sitter-hl-face:variable.builtin
-  ;;                      (face-foreground 'font-lock-keyword-face))
-
   )
 
 (use-package tree-sitter-langs
