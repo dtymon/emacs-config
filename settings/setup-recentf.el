accbf7f24824a4d714b6874386a0da33c190bed2
David Tymon <david.tymon@gmail.com>
2025-02-10 15:55:12 +1100

    Bind C-x C-r to do a recentf-open

--- settings/setup-recentf.el
+++ settings/setup-recentf.el
@@ -1,13 +1,18 @@
 ;; Save a list of recent files visited. (open recent file with C-x f)
 (use-package recentf
   :init
-  (setq recentf-max-saved-items 100)
+  (setq recentf-max-saved-items 500)
   :config
   (recentf-mode 1)
 
   ;; Do not include any elpa files and other state-related files
   (add-to-list 'recentf-exclude "/\\.emacs\\.d/\\(var\\|elpa\\|\\.cache\\)/")
   (add-to-list 'recentf-exclude "^/private/var/")
+
+  (let ((map global-map))
+    (define-key map (kbd "C-x C-S-r") #'find-file-read-only)
+    (define-key map (kbd "C-x C-r") #'recentf-open)
+    )
   )
 
 (provide 'setup-recentf)
