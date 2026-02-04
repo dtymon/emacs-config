26e0824e22580ceb6521dbfbc4ee858d6bacafe1
David Tymon <david.tymon@gmail.com>
2026-02-04 13:16:11 +1100

    Tweaks to vterm

--- settings/setup-vterm.el
+++ settings/setup-vterm.el
@@ -3,11 +3,41 @@
 (use-package vterm
   :ensure t
   :bind (:map vterm-mode-map
-              ("C-c C-x" . vterm--self-insert)
-              ("C-x <prior>" . vterm-copy-mode)
+              ("C-c C-x"     . #'vterm--self-insert)
+              ("C-c M-l"     . #'vterm-clear-scrollback)
+              ("C-x <prior>" . #'vterm-copy-mode)
+              ("C-q"         . #'vterm-send-next-key)
+
+              :map vterm-copy-mode-map
+              ("C-c C-o" . #'find-file-at-point)
+              ("C-c f" . #'dtymon::vterm-insert-e-at-point)
               )
+
   :config
+  (setq
+   vterm-shell "zsh -l"
+   vterm-max-scrollback 100000
+   )
   ;; (setq term-prompt-regexp "^\nzsh .*\n[0-9][0-9]:[0-9][0-9][AP]M .*> ")
+
+  (add-to-list 'vterm-eval-cmds '("find-file-line"
+                                  (lambda (file line)
+                                    (let ((line-num (string-to-number line)))
+                                      (find-file-other-window file)
+                                      (goto-char (point-min))
+                                      (goto-line line-num)))))
+
+  (defun dtymon::vterm-insert-e-at-point (&optional no-return)
+    (interactive "P")
+    (let ((file (thing-at-point 'filename t)))
+      (unless file
+        (user-error "No filename at point"))
+      ;; leave copy-mode so input goes to the terminal
+      (vterm-copy-mode -1)
+      ;; send command
+      (vterm-send-string (concat " e " file))
+      (unless no-return
+        (vterm-send-return))))
 
   (defun dtymon::vterm-turn-off-chrome ()
     (hl-line-mode -1)
@@ -21,11 +51,11 @@
 ;;   :custom
 ;;   (vterm-toggle-fullscreen-p nil "Open a vterm in another window.")
 ;;   (vterm-toggle-scope 'project)
-;;   :bind (
-;;          ("C-c t" . #'vterm-toggle)
-;;          :map vterm-mode-map
-;;          ("s-t" . #'vterm) ; Open up new tabs quickly
-;;          )
+;;   ;; :bind (
+;;   ;;        ("C-c t" . #'vterm-toggle)
+;;   ;;        :map vterm-mode-map
+;;   ;;        ("s-t" . #'vterm) ; Open up new tabs quickly
+;;   ;;        )
 ;;   )
 
 (provide 'setup-vterm)
